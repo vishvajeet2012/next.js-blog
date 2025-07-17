114a150e0e110ebd8bfefd35304fe03f850059ca
vishu <djritikshukla@gmai.com>
2025-07-17 15:09:52 +0530

    mirror changes

--- src/components/blog/CreateBlog.js
+++ src/components/blog/CreateBlog.js
@@ -136,7 +136,6 @@ function CreateBlogForm({ user }) {
     );
   };
 
-  console.log(title, category, coverImage, content);
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
